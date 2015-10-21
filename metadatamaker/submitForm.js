5bc2ef067571f2775cfa1872ae48514172ecb583
Deren Kudeki <dkudeki@illinois.edu>
2015-10-21 12:26:35 -0500

    Revert "Merge branch 'version-1.1' into Theses-&-Dissertations"
    This reverts commit 75e396210fa211f1fe98f78e1783eb05d8a5d856, reversing
    changes made to ee1861936bee146cadbe38d9661e4fec5a0d3ec7.

--- metadatamaker/submitForm.js
+++ metadatamaker/submitForm.js
@@ -1,19 +1,10 @@
-/*
- *	Reads the 'name' variable from the URL and returns the value. Used for passing custom institution info.
- *		if the value has not been passed the function will return undefined.
- *
- *	name: String with the name of an expected variable in the URL
- */
 function get(name) {
 	if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search)) {
 		return decodeURIComponent(name[1]);
 	}
 }
 /* 
- * Edit the strings in this output to attribute records to another institution. The second half of the
- *		function checks the URL for custom info. If that info exists, it overwrites the defaults.
- *
- *	Returns the institution info
+ * Edit the strings in this function to attribute records to another institution
  */
 function generateInstitutionInfo() {
 	var output = {
@@ -33,42 +24,6 @@ function generateInstitutionInfo() {
 	marc = get('marc');
 	if (typeof marc !== 'undefined') {
 		output['marc'] = marc;
-	}
-	physicalLocation = get('physicalLocation');
-	if (typeof physicalLocation !== 'undefined') {
-		output['mods']['physicalLocation'] = physicalLocation;
-	}
-	recordContentSource = get('recordContentSource');
-	if (typeof recordContentSource !== 'undefined') {
-		output['mods']['recordContentSource'] = recordContentSource;
-	}
-	lcn = get('lcn');
-	if (typeof lcn !== 'undefined') {
-		output['html']['url']  = 'http://id.loc.gov/authorities/names/' + lcn;
-	}
-	n = get('n');
-	if (typeof n !== 'undefined') {
-		output['html']['name'] = n;
-	}
-
-	return output;
-}
-
-/*
- * The first listed author should be placed in 100. If no author is listed, then the first
- * listed artist should be placed in 100. If neither role is listed, then we return a person
- * with no name or role listed.
- *
- * list: List of people. Each person is a list of two objects. The first object contains the
- *		 person's family name, given name, and role in creating the piece being catalogued.
- *		 The second object contains the transliterated family name and given name of the same
- *		 person if applicable. Otherwise those two fields are empty strings.
- */
-function find100(list) {
-	for (iterator = 0; iterator < list.length; iterator++) {
-		if (list[iterator][0]['role'] == 'aut') {
-			return list.splice(iterator,1);
-		}
 	}
 	physicalLocation = get('physicalLocation');
 	if (typeof physicalLocation !== 'undefined') {
