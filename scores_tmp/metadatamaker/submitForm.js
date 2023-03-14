a4a7403a2870b28365664eea1dd280df2aa2f02c
Deren Kudeki <dkudeki@illinois.edu>
2023-03-14 17:30:27 -0500

    Remove scores submodule, start local folder for scores page

---
+++ scores_tmp/metadatamaker/submitForm.js
@@ -0,0 +1,212 @@
+/*
+ *	Reads the 'name' variable from the URL and returns the value. Used for passing custom institution info.
+ *		if the value has not been passed the function will return undefined.
+ *
+ *	name: String with the name of an expected variable in the URL
+ */
+function get(name) {
+	if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search)) {
+		return decodeURIComponent(name[1]);
+	}
+}
+/* 
+ * Edit the strings in this output to attribute records to another institution. The second half of the
+ *		function checks the URL for custom info. If that info exists, it overwrites the defaults.
+ *
+ *	Returns the institution info
+ */
+function generateInstitutionInfo() {
+	var output = {
+		//040 $a, 040 $c
+		marc: 'UIU',
+		mods: {
+			physicalLocation: 'University of Illinois at Urbana-Champaign, Library',
+			recordContentSource: 'UIU'
+		},
+		//"seller" info
+		html: {
+			url: 'http://id.loc.gov/authorities/names/n79066210',
+			name: 'University of Illinois at Urbana-Champaign'
+		}
+	};
+
+	marc = get('marc');
+	if (typeof marc !== 'undefined') {
+		output['marc'] = marc;
+	}
+	physicalLocation = get('physicalLocation');
+	if (typeof physicalLocation !== 'undefined') {
+		output['mods']['physicalLocation'] = physicalLocation;
+	}
+	recordContentSource = get('recordContentSource');
+	if (typeof recordContentSource !== 'undefined') {
+		output['mods']['recordContentSource'] = recordContentSource;
+	}
+	lcn = get('lcn');
+	if (typeof lcn !== 'undefined') {
+		output['html']['url']  = 'http://id.loc.gov/authorities/names/' + lcn;
+	}
+	n = get('n');
+	if (typeof n !== 'undefined') {
+		output['html']['name'] = n;
+	}
+
+	return output;
+}
+
+/*
+ * The first listed author should be placed in 100. If no author is listed, then the first
+ * listed artist should be placed in 100. If neither role is listed, then we return a person
+ * with no name or role listed.
+ *
+ * list: List of people. Each person is a list of two objects. The first object contains the
+ *		 person's family name, given name, and role in creating the piece being catalogued.
+ *		 The second object contains the transliterated family name and given name of the same
+ *		 person if applicable. Otherwise those two fields are empty strings.
+ */
+function find100(list) {
+	for (iterator = 0; iterator < list.length; iterator++) {
+		if (list[iterator][0]['role'] == 'aut') {
+			return list.splice(iterator,1);
+		}
+	}
+
+	for (iterator = 0; iterator < list.length; iterator++) {
+		if (list[iterator][0]['role'] == 'art') {
+			return list.splice(iterator,1);
+		}
+	}
+
+	for (iterator = 0; iterator < list.length; iterator++) {
+		if (list[iterator][0]['role'] == 'cmp') {
+			return list.splice(iterator,1);
+		}
+	}
+
+	for (iterator = 0; iterator < list.length; iterator++) {
+		if (list[iterator][0]['role'] == 'lbt') {
+			return list.splice(iterator,1);
+		}
+	}	
+
+	return [[{'family':'','given':'','role':''},{'family':'','given':''}]];
+}
+
+/*
+ * When the form is submitted, create an object with all user-submitted data. Pass that object to functions that
+ * build a record around the data.
+ *
+ * The first listed author or artist is placed in recordObject.author, while all other credited individuals are
+ * placed into recordObject.additional_authors.
+ *
+ * No information should be submitted to the server, so the default behavior of the button is blocked.
+ */
+$("#marc-maker").submit(function(event) {
+	var words = [];
+	var fast_array = [];
+	for (var i = 0; i < counter; i++) {
+		if(checkExists($("#fastID" + i).val()) && checkExists($("#keyword" + i).val())) {
+			if ($("#keyword" + i).val().substring($("#keyword" + i).val().length - 1) == ']') {
+				var endpoint = $("#keyword" + i).val().lastIndexOf('[');
+				fast_array.push([$("#keyword" + i).val().substring(0,endpoint-1),$("#fastID" + i).val(),$("#fastType" + i).val(),$("#fastInd" + i).val()]);
+			}
+			else {
+				fast_array.push([$("#keyword" + i).val(),$("#fastID" + i).val(),$("#fastType" + i).val(),$("#fastInd" + i).val()]);
+			}
+		}
+		else {
+			words.push($("#keyword" + i).val());
+		}
+	};
+
+	var additional_names = [];
+	var translit_additional_names = [];
+	var complete_names_list = [
+		[
+			{
+				family: $("#family_name").val(),
+				given: $("#given_name").val(),
+				role: $("#role").val()
+			},
+			{
+				family: $("#translit_family_name").val(),
+				given: $("#translit_given_name").val()
+			}
+		]
+	];
+	for (var i = 0; i < aCounter; i++) {
+		complete_names_list.push([{ "family": $("#family_name" + i).val(), "given": $("#given_name" + i).val(), "role": $("#role" + i).val()},{ "family": $("#translit_family_name" + i).val(), "given": $("#translit_given_name" + i).val()}]);
+	}
+	//Find the first listed author or artist
+	var entry100 = find100(complete_names_list);
+
+	var accompanying_matter_selections = [];
+	for (var i = 0; i <= 13; i++) {
+		if($("#accompanying-matter" + i.toString()).is(":checked")) {
+			accompanying_matter_selections.push($("#accompanying-matter" + i.toString()).val());
+		}
+	}
+
+	var recordObject = {
+		title: [
+			{
+				title: $("#title").val(),
+				subtitle: $("#subtitle").val()
+			},
+			{
+				title: $("#translit_title").val(),
+				subtitle: $("#translit_subtitle").val()
+			}
+		],
+		uniform_title: $("#uniform_title").val(),
+		author: entry100[0],
+		publisher: $("#publisher").val(),
+		publication_year: $("#year").val(),
+		publication_place: $("#place").val(),
+		publication_country: $("#country").val(),
+		copyright_year: $("#cyear").val(),
+		language: $("#language").val(),
+		isbn: $("#isbn").val(),
+		ismn: $("#ismn").val(),
+		volume_or_page: $("#vorp").val(),
+		pages: $("#pages").val(),
+		unpaged: $("#pages_listed").is(':checked'),
+		literature_yes: $("#literature-yes").is(':checked'),
+		literature_dropdown: $("#literature-dropdown").val(),
+		illustrations_yes: $("#illustrations-yes").is(':checked'),
+		dimensions: $("#dimensions").val(),
+		edition: $("#edition").val(),
+		composition_form: $("#composition-form-dropdown").val(),
+		music_parts: $("#music-parts-dropdown").val(),
+		transposition_arrangement: $("#transposition-arrangement-dropdown").val(),
+		accompanying_matters: accompanying_matter_selections,
+		translit_edition: $("#translit_edition").val(),
+		translit_publisher: $("#translit_publisher").val(),
+		translit_place: $("#translit_place").val(),
+		notes: $("#notes").val(),
+		formatted_contents_note: $("#formatted-contents-note").val(),
+		keywords: words,
+		fast: fast_array,
+		additional_authors: complete_names_list
+	};
+
+	var institution_info = generateInstitutionInfo();
+
+	if ($("#MARC").is(':checked')) {
+		downloadMARC(recordObject,institution_info);
+	}
+
+	if ($("#MARCXML").is(':checked')) {
+		downloadXML(recordObject,institution_info);
+	}
+
+	if ($("#MODS").is(':checked')) {
+		downloadMODS(recordObject,institution_info);
+	}
+
+	if ($("#HTML").is(':checked')) {
+		downloadHTML(recordObject,institution_info);
+	}
+
+	event.preventDefault();
+});
