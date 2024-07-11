f7c7346432c445e2bfb921efe0c7572430a8d5a6
mjhan3 <mhan3@illinois.edu>
2024-07-11 07:56:24 -0500

    Update lcshsearch.js
    Change the service to API.

--- monoviaf/LCSH/lcshsearch.js
+++ monoviaf/LCSH/lcshsearch.js
@@ -1,11 +1,12 @@
-$(function() {
+/* $(function() {
 	document.getElementById('LCSHSuggest').onclick = function(){
 		document.getElementById("LCSHresponse").innerHTML = "";
 		var summary = document.getElementById('summary').value;
 		if (summary!=null){
-			var requests = "text=" + summary;
+			var requests = summary; //"text=" + summary;
 			// var url = "https://annif.info/v1/projects/ivyplus-tfidf/suggest";
-			var url = "https://annif.info/v1/projects/upenn-omikuji-bonsai-en-gen/suggest";
+			// var url = "https://annif.info/v1/projects/upenn-omikuji-bonsai-en-gen/suggest";
+			var url = "https://ai.finto.fi/v1/projects/yso-en/suggest";
 			var xhr = new XMLHttpRequest();
 			xhr.open("POST", url, false);
 			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -27,6 +28,45 @@ $(function() {
 			xhr.send(requests);
 		}
 	};
+}); 
+*/
+
+$(document).ready(function() {
+    $('#LCSHSuggest').click(function() {
+        $('#LCSHresponse').empty(); 
+        var summary = $('#summary').val();
+        if (summary.trim() !== "") { 
+            var url = "https://api.annif.org/v1/projects/yso-en/suggest";
+            var requestData = {
+                text: summary,
+                limit: 10
+            };
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: requestData,
+                dataType: "json",
+                success: function(response) {
+                    console.log(response);
+                    $('#LCSHresponse').empty(); 
+                    if (response.results && response.results.length > 0) {
+                        for (var i = 0; i < response.results.length; i++) {
+                            var lcshLabel = response.results[i].label;
+                            var lcshUrl = response.results[i].uri;
+                            $('#LCSHresponse').append('<input type="checkbox" name="lcsh" class="lcshcheckbox" uri="' + lcshUrl + '" value="' + lcshLabel + '">' + lcshLabel + '<br>');
+                        }
+                    } else {
+                        $('#LCSHresponse').html('No results found.');
+                    }
+                },
+                error: function(xhr, status, error) {
+                    console.error("Error:", error);
+                    $('#LCSHresponse').html('Error occurred while fetching suggestions.');
+                }
+            });
+        } else {
+            $('#LCSHresponse').html('Please enter a summary.');
+        }
+    });
 });
-
-
