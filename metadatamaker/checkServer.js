d8893d52d0809da70512581a045fcd0a882aca22
Deren Kudeki <dkudeki@illinois.edu>
2026-04-14 22:37:07 -0500

    Move warnings to top to stop blocking elements and place them within a structure. Also ensure noscript warning is always visible when needed

--- metadatamaker/checkServer.js
+++ metadatamaker/checkServer.js
@@ -10,8 +10,9 @@ var servername = window.location.href ;
 if (servername.indexOf('metadatamaker.library.illinois.edu') < 0) {
 	console.log('WRONG SERVER');
 	console.log(servername)
-	var newdiv = document.createElement('span');
+	var newdiv = document.createElement('div');
 	newdiv.setAttribute('id','server_warning');
+	newdiv.setAttribute('class','warning');
 	newdiv.innerHTML = 'WARNING: This version of Metadata Maker is under development and may be unstable. The official version of Metadata Maker can be found <a href="' + buildRedirectURL(servername) + '">here</a>.';
-	$("body").append(newdiv);
+	$("header").prepend(newdiv);
 }
